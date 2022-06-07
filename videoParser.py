e0f23441b59fef2e07e76069a2bb78f34565e4fb
DESKTOP-0191A3N\Devin <DevinQi@outlook.com>
2022-06-07 01:25:53 -0400

    function templet and code cleaning

--- videoParser.py
+++ videoParser.py
@@ -6,18 +6,32 @@ import pdb
 # set np print with no limitations
 np.set_printoptions(threshold=np.inf)
 
+# 1. 先图像灰度的求平均值， 找到异常小的值，把灰度图转化为0和1的异常图
+# 2. 找出聚集在一起的异常点，可以同过面积进行筛选噪声点
+# 3. particle的中心可以用 (x_difference/2, y_difference / 2) 来估计
+def locateParticals(img):
+    return []
 
-def processVideo(path, timeGap):
+
+def analyzeMovements(imgPre,imgCur):
+    return []
+
+def processVideo(path, frameGap):
     cap = cv2.VideoCapture(path)
     fps = math.ceil(cap.get(cv2.CAP_PROP_FPS))
     totalFrame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    gapFrames = fps / timeGap
+
+    print("====================================")
+    print("Start processing video " + path)
+    print("FPS: " + str(fps))
+    print("Total Frams: " + str(totalFrame))
+    print("Gap Frames: " + str(frameGap))
 
     # processing video
     frameCount = 0
     while cap.isOpened():
 
-        if frameCount % gapFrames == 0:
+        if frameCount % frameGap == 0:
             ret, frame = cap.read()
             # covert to gray scale
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -33,4 +47,4 @@ def processVideo(path, timeGap):
     cv2.destroyAllWindows()
 
 
-processVideo("1.mp4")
+processVideo("1.mp4", 6)
